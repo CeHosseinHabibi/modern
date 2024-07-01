5ad79f563630b600ae64b72a5ad8e99c97f52cb6
H.Habibi <hosseinhabibi73@gmail.com>
2024-07-01 17:57:13 +0330

    Corrected RestTemplateClient.callRollBack

--- src/main/java/com/habibi/modern/client/RestTemplateClient.java
+++ src/main/java/com/habibi/modern/client/RestTemplateClient.java
@@ -1,10 +1,10 @@
 package com.habibi.modern.client;
 
+import com.habibi.modern.dto.RollBackWithdrawResponseDto;
 import com.habibi.modern.dto.RollbackWithdrawDto;
 import com.habibi.modern.dto.WithdrawDto;
 import com.habibi.modern.dto.WithdrawResponseDto;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,14 +20,12 @@ public class RestTemplateClient {
     private Long registrationFee;
 
     public WithdrawResponseDto callWithdraw(Long accountNumber) {
-        ResponseEntity<WithdrawResponseDto> withdrawResponseDtoResponseEntity =
-                restTemplate.postForEntity(CORE_SERVICE_URL + SLASH + ACCOUNTS + SLASH + WITHDRAW,
-                        new WithdrawDto(accountNumber, registrationFee), WithdrawResponseDto.class);
-        return withdrawResponseDtoResponseEntity.getBody();
+        return restTemplate.postForEntity(CORE_SERVICE_URL + SLASH + ACCOUNTS + SLASH + WITHDRAW,
+                new WithdrawDto(accountNumber, registrationFee), WithdrawResponseDto.class).getBody();
     }
 
-    public void callRollBack(RollbackWithdrawDto rollbackWithdrawDto) {
-        restTemplate.postForEntity(CORE_SERVICE_URL + SLASH + ACCOUNTS + SLASH + ROLLBACK_WITHDRAW,
-                rollbackWithdrawDto, Void.class);
+    public RollBackWithdrawResponseDto callRollBack(RollbackWithdrawDto rollbackWithdrawDto) {
+        return restTemplate.postForEntity(CORE_SERVICE_URL + SLASH + ACCOUNTS + SLASH + ROLLBACK_WITHDRAW,
+                rollbackWithdrawDto, RollBackWithdrawResponseDto.class).getBody();
     }
 }
---
+++ src/main/java/com/habibi/modern/dto/RollBackWithdrawResponseDto.java
@@ -0,0 +1,17 @@
+package com.habibi.modern.dto;
+
+import com.habibi.modern.enums.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RollBackWithdrawResponseDto {
+    private UUID trackingCode;
+    private ErrorCode errorCode;
+    private String description;
+}
