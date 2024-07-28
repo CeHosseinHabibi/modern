37c7cb45df5227f9b03fc0d230f1a4d8054c6617
H.Habibi <hosseinhabibi73@gmail.com>
2024-07-28 14:44:49 +0330

    Added EnableFeignClients

--- src/main/java/com/habibi/modern/ModernApplication.java
+++ src/main/java/com/habibi/modern/ModernApplication.java
@@ -2,12 +2,14 @@ package com.habibi.modern;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ModernApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ModernApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ModernApplication.class, args);
+    }
 
 }
